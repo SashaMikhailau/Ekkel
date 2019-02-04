0071a58b3154f291a419fadf673f8da5e2744019
SashaMikhailau <azyainhome@gmail.com>
2019-02-04 12:36:20 +0300

    Auto stash before rebase of "origin/master"

---
+++ src/arrays/ex11/AutoStore.java
@@ -0,0 +1,21 @@
+package arrays.ex11;
+
+public class AutoStore {
+    public static void main(String[] args) {
+        int[] ints = new int[5];
+        //getArray(ints);
+        Integer[] integer= new Integer[] {1,2,3,4,5};
+        //getArrayPrim(integer);
+        getArrayPrim(ints);
+        getArray(integer);
+
+
+    }
+
+    public static void getArray(Integer[] array) {
+
+    }
+    public static void getArrayPrim(int[] array) {
+
+    }
+}
